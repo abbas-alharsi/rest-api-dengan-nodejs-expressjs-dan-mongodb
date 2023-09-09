f94f76ec70699bf5f63829d914032bdf0c5cc0dd
abbas-alharsi <66052081+abbas-alharsi@users.noreply.github.com>
2023-09-09 22:24:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
-const cors = require('cors')
 const path = require('path')
+const cors = require('cors')
 const {MongoClient, ObjectId} = require('mongodb')
 
 const url = 'mongodb://127.0.0.1:27017'
@@ -12,11 +12,8 @@ app.use(cors())
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-app.get('/rest-api', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))
-} )
-async function getProducts(res, id = false) {
-    try {
+async function getProduct(res, id = false) {
+    try{
         await client.connect()
         if(id) {
             const data = await client.db('rest-api').collection('products').find({_id: new ObjectId(id)}).sort({'_id':-1}).toArray()
@@ -25,41 +22,37 @@ async function getProducts(res, id = false) {
             const data = await client.db('rest-api').collection('products').find({}).sort({'_id':-1}).toArray()
             res.send(data)
         }
+        
     } finally {
         client.close()
     }
 }
-app.get('/rest-api/data', (req, res) => {
-    getProducts(res)
-})
-
-app.post('/rest-api/insert', (req, res) => {
-    const productData = req.body
-    async function insertProduct(){
-        try{
-            await client.connect()
-            await client.db('rest-api').collection('products').insertOne({product: productData.productName, category: productData.category, price: productData.price, qty: productData.qty})
-            res.send('<h1>Data added to database</h1>')
-        } finally {
-            client.close()
-        }
-    }
-    insertProduct()
-})
 
 app.get('/rest-api/products/:id', (req, res) => {
-    let id = req.params.id
-    getProducts(res, id)
+    const id = req.params.id
+    getProduct(res, id)
 })
 
 app.post('/rest-api/products/update/:id', (req, res) => {
     const id = req.params.id
     const productData = req.body
-    async function updateProduct(){
+    async function updateProduct() {
         try{
             await client.connect()
-            await client.db('rest-api').collection('products').updateOne({_id: new ObjectId(id)}, {$set: {product: productData.newProductName, category: productData.newCategory, price: productData.newPrice, qty: productData.newQty}})
-            res.send('<h1>Product Data is updated</h1>')
+            await client.db('rest-api').collection('products').updateOne(
+                {
+                    _id: new ObjectId(id)
+                },
+                {
+                    $set: {
+                        product: productData.newProductName,
+                        category: productData.newCategory,
+                        price: productData.newPrice,
+                        qty: productData.newQty
+                    }
+                }
+            )
+            res.send('<h1>Data updated</h1>')
         } finally {
             client.close()
         }
@@ -69,11 +62,11 @@ app.post('/rest-api/products/update/:id', (req, res) => {
 
 app.post('/rest-api/products/delete/:id', (req, res) => {
     const id = req.params.id
-    async function deleteProduct(){
+    async function deleteProduct() {
         try{
             await client.connect()
             await client.db('rest-api').collection('products').deleteOne({_id: new ObjectId(id)})
-            res.send('<h1>Product Data is Deleted</h1>')
+            res.send('<h1>Data Deleted</h1>')
         } finally {
             client.close()
         }
@@ -81,6 +74,36 @@ app.post('/rest-api/products/delete/:id', (req, res) => {
     deleteProduct()
 })
 
+app.get('/rest-api/data', (req, res) => {
+    getProduct(res)
+})
+
+app.post('/rest-api/insert', (req, res) => {
+    const productData = req.body
+    async function insertProduct() {
+        try{
+            await client.connect()
+            await client.db('rest-api').collection('products').insertOne(
+                {
+                    product: productData.productName,
+                    category: productData.category,
+                    price: productData.price,
+                    qty: productData.qty
+                }
+            )
+            res.send('<h1>Data added to database</h1>')
+        } finally {
+            client.close()
+        }
+    }
+    insertProduct()
+})
+
+app.get('/rest-api', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+
 app.listen(port, () => {
-    console.log(`server listening on port ${port}`)
+    console.log(`listening on port ${port}`)
 })
